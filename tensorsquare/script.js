85912d1fc012a72ee2d823cb855553398cc83ae0
Ucchishta Sivaguru <ucc.men@gmail.com>
2023-02-12 16:11:50 +0800

    update square data with tensorflow files

---
+++ tensorsquare/script.js
@@ -0,0 +1,34 @@
+/**
+ * Get the car data reduced to just the variables we are interested
+ * and cleaned of missing data.
+ */
+async function getData() {
+  const squaresDataResponse = await fetch('https://raw.githubusercontent.com/uccmen/countmore/master/square-data.json');
+  const squaresData = await squaresDataResponse.json();
+
+  return squaresData;
+}
+
+
+async function run() {
+  // Load and plot the original input data that we are going to train on.
+  const data = await getData();
+  const values = data.map(d => ({
+    x: d.squareOf,
+    y: d.answer,
+  }));
+
+  tfvis.render.scatterplot(
+    {name: 'SquareOf v Answer'},
+    {values},
+    {
+      xLabel: 'Square',
+      yLabel: 'Answer',
+      height: 300
+    }
+  );
+
+  // More code will be added below
+}
+
+document.addEventListener('DOMContentLoaded', run);
