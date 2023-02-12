c95781fe06bf64d878a7ec0a63cbe1c46f27bbf6
Ucchishta Sivaguru <ucc.men@gmail.com>
2023-02-12 16:43:38 +0800

    random upwards data

--- tensorsquare/script.js
+++ tensorsquare/script.js
@@ -3,13 +3,8 @@
  * and cleaned of missing data.
  */
 async function getData() {
-  const squaresDataResponse = await fetch('https://raw.githubusercontent.com/uccmen/countmore/master/square-data-6-16.json');
+  const squaresDataResponse = await fetch('https://raw.githubusercontent.com/uccmen/countmore/master/random-upwards-data.json');
   const squaresData = await squaresDataResponse.json();
-  squaresData.map(squareData => {
-    if (squareData.squareOf < 6) {
-      console.log('SQUARE OF IS LESS THAN 6', squareData.squareOf);
-    }
-  });
 
   return squaresData;
 }
@@ -19,8 +14,8 @@ async function run() {
   // Load and plot the original input data that we are going to train on.
   const data = await getData();
   const values = data.map(d => ({
-    x: d.squareOf,
-    y: d.answer,
+    x: d.x,
+    y: d.y,
   }));
 
   tfvis.render.scatterplot(
