3803e362c555c88f7e565f182b6e01cde607a8b2
Ucchishta Sivaguru <ucc.men@gmail.com>
2023-02-12 17:04:57 +0800

    update random upwards data

--- tensorsquare/script.js
+++ tensorsquare/script.js
@@ -3,7 +3,7 @@
  * and cleaned of missing data.
  */
 async function getData() {
-  const squaresDataResponse = await fetch('https://raw.githubusercontent.com/uccmen/countmore/master/random-upwards-data-1.json');
+  const squaresDataResponse = await fetch('https://raw.githubusercontent.com/uccmen/countmore/master/square-data.json');
   const squaresData = await squaresDataResponse.json();
   console.log('DATA', squaresData);
 
@@ -15,16 +15,16 @@ async function run() {
   // Load and plot the original input data that we are going to train on.
   const data = await getData();
   const values = data.map(d => ({
-    x: d.x,
-    y: d.y,
+    x: d.squareOf,
+    y: d.answer,
   }));
 
   tfvis.render.scatterplot(
-    {name: 'SquareOf v Answer'},
+    {name: 'Answer vs Square Of'},
     {values},
     {
-      xLabel: 'Square',
-      yLabel: 'Answer',
+      xLabel: 'Answer',
+      yLabel: 'Square Of',
       height: 300
     }
   );
